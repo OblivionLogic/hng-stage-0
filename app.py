c4d8a3eec7ccc1fefefece62318e9b5301b6e936
OblivionLogic <ekpo2419@gmail.com>
2026-04-18 23:18:05 +0100

    Initialize Flask app with info endpoint

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+from datetime import datetime
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods=['GET'])
+def get_info():
+    return jsonify({
+        "email": "your-email@example.com",
+        "current_datetime": datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
+        "github_url": "https://github.com"
+    }), 200
+
+if __name__ == '__main__':
+    app.run()
