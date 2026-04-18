857bc975b728370624cab64faf1ccbdd93f80c72
OblivionLogic <ekpo2419@gmail.com>
2026-04-18 23:35:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, jsonify
 from datetime import datetime
 from flask_cors import CORS
@@ -7,10 +8,18 @@ CORS(app)
 
 @app.route('/', methods=['GET'])
 def get_info():
+    # Fetch random cat fact
+    try:
+        fact_response = requests.get("https://catfact.ninja/fact")
+        cat_fact = fact_response.json().get('fact', 'No fact found')
+    except:
+        cat_fact = "Could not fetch cat fact"
+
     return jsonify({
         "email": "dicekhandy@gmail.com",
         "current_datetime": datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
-        "github_url": "https://github.com"
+        "github_url": "https://github.com",
+        "fact": cat_fact
     }), 200
 
 if __name__ == '__main__':
